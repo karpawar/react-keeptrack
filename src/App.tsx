ac807f5ae10e0b8a8d6be4fe253734bc0f98a55b
Karan P <karan.pawar@coditas.com>
2023-05-11 17:43:10 +0530

    track of all keep list

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import ProjectPage from "./projects/ProjectPage";
 
 function App() {
   return (
-    <blockquote cite="Benjamin Franklin">
-          Tell me and I forget, teach me and I may remember, involve me and I learn.
-    </blockquote>
+    <div className="container">
+      <ProjectPage />
+    </div>
   );
 }
 
