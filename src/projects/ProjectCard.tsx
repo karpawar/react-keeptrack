6e5f7ad52fdac9aa6c322a7828b795d654a97d76
Karan P <karan.pawar@coditas.com>
2023-05-13 00:33:08 +0530

    updated form basic case

--- src/projects/ProjectCard.tsx
+++ src/projects/ProjectCard.tsx
@@ -1,8 +1,8 @@
-import { Project } from './Project';
-import React from 'react';
+import { Project } from "./Project";
+import React from "react";
 
 function formatDescription(description: string): string {
-  return description.substring(0, 60) + '...';
+  return description.substring(0, 60) + "...";
 }
 
 interface ProjectCardProps {
@@ -11,6 +11,10 @@ interface ProjectCardProps {
 
 function ProjectCard(props: ProjectCardProps) {
   const { project } = props;
+  const handleEditClick = (projectBeingEdited: Project) => {
+    console.log(projectBeingEdited);
+  };
+
   return (
     <div className="card">
       <img src={project.imageUrl} alt={project.name} />
@@ -20,6 +24,15 @@ function ProjectCard(props: ProjectCardProps) {
         </h5>
         <p>{formatDescription(project.description)}</p>
         <p>Budget : {project.budget.toLocaleString()}</p>
+        <button
+          className="bordered"
+          onClick={() => {
+            handleEditClick(project);
+          }}
+        >
+          <span className="icon-edit "></span>
+          Edit
+        </button>
       </section>
     </div>
   );
